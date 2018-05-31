6b40920e629a7d5688fe935e72d2ae7ac9aa6098
Rabin OJHA <rabinkumarojha@gmail.com>
2018-06-01 00:48:09 +0200

    first Day

---
+++ app.js
@@ -0,0 +1,30 @@
+const express=require('express');
+const app=express();
+const path =require('path');
+const exphbs=require('express-handlebars');
+
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.engine('handlebars',exphbs({defaultLayout:'home'}));
+app.set('view engine','handlebars');
+
+app.get('/',(req,res)=>{
+  res.render('home/index');
+});
+
+app.get('/about',(req,res)=>{
+  res.render('home/about');
+});
+
+app.get('/login',(req,res)=>{
+  res.render('home/login');
+});
+
+app.get('/register',(req,res)=>{
+  res.render('home/register');
+});
+
+app.listen(4500,()=>{
+  console.log(`listening on port 4500`);
+});
